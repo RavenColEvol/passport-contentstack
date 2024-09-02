45015081205c3fa131dd154f50179a5b2d75f67e
Faraaz Biyabani <faraaz.biyabani@contentstack.com>
2024-09-02 16:20:43 +0530

    feat: add GCP_NA region

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,9 @@
 export enum Regions {
   NA = "NA",
   EU = "EU",
-  AZURE_NA = 'AZURE_NA',
-  AZURE_EU = 'AZURE_EU'
+  AZURE_NA = "AZURE_NA",
+  AZURE_EU = "AZURE_EU",
+  GCP_NA = "GCP_NA",
 }
 
 export const regionBaseURLs = {
@@ -10,6 +11,7 @@ export const regionBaseURLs = {
   [Regions.EU]: "https://eu-app.contentstack.com",
   [Regions.AZURE_NA]: "https://azure-na-app.contentstack.com",
   [Regions.AZURE_EU]: "https://azure-eu-app.contentstack.com",
+  [Regions.GCP_NA]: "https://gcp-na-app.contentstack.com",
 };
 
 export const regionAPIBaseURLs = {
@@ -17,7 +19,8 @@ export const regionAPIBaseURLs = {
   [Regions.EU]: "https://eu-api.contentstack.com",
   [Regions.AZURE_NA]: "https://azure-na-api.contentstack.com",
   [Regions.AZURE_EU]: "https://azure-eu-api.contentstack.com",
-}
+  [Regions.GCP_NA]: "https://gcp-na-api.contentstack.com",
+};
 
 export interface User {
   uid: string;
